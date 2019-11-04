4770a10d71d33bdcb5c57331f7ff2a9a493cabe7
dexterchan <dexterchan@gmail.com>
2019-11-04 18:51:15 +0800

    add autowire

--- cryptosubscriber/src/main/java/io/beam/exp/CryptoSubscriberController.java
+++ cryptosubscriber/src/main/java/io/beam/exp/CryptoSubscriberController.java
@@ -3,6 +3,7 @@ package io.beam.exp;
 
 import io.beam.exp.service.CryptoSubscriberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/crypto")
 public class CryptoSubscriberController {
 
-    private final CryptoSubscriberService svc;
+    @Autowired
+    private final CryptoSubscriberService cryptoSubscriberService;
 
     @RequestMapping("/")
     public String helloworld() {
@@ -22,13 +24,13 @@ public class CryptoSubscriberController {
 
     @GetMapping("/startservice")
     public String startService(@RequestParam String baseCcy, @RequestParam String counterCcy){
-        svc.startSubscription("",baseCcy,counterCcy);
+        cryptoSubscriberService.startSubscription("",baseCcy,counterCcy);
         return "subscription started";
     }
 
     @GetMapping("/stopservice")
     public String stopService(@RequestParam String baseCcy, @RequestParam String counterCcy){
-        svc.stopSubscription("",baseCcy,counterCcy);
+        cryptoSubscriberService.stopSubscription("",baseCcy,counterCcy);
         return "subscription stopped";
     }
 }
